0f6fe534903824593df3752501cbad0317822941
汪心禾 <wangxinhe06@gmail.com>
2020-06-09 19:25:56 +0800

    Bilibili.com domain

--- mdl/extractor/__init__.py
+++ mdl/extractor/__init__.py
@@ -4,5 +4,5 @@ from . import bilibili
 
 
 extractors = {
-    'bilibili': [re.compile(r'(av[0-9]|BV[0-9A-Za-z])'), bilibili.video]
+    'bilibili': [re.compile(r'^(?:https?://(?:www.)?bilibili.com/(?:video/)?)?(av[0-9]+|BV[0-9A-Za-z]+)'), bilibili.video]
 }
--- test_.py
+++ test_.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 
 import mdl
@@ -15,14 +16,43 @@ class TestDownloader(unittest.TestCase):
 
 class TestBiliVideo(unittest.TestCase):
 
-    def assertion(self, vid):
-        self.assertEqual(set(os.listdir('av7')), {'audio', 'video'})
-        self.assertGreater(len(os.listdir('av7/video')), 0)
-        self.assertGreater(len(os.listdir('av7/audio')), 0)
+    def assertion(self, vid, videos=1, audios=1):
+        self.assertEqual(set(os.listdir(vid)), {'audio', 'video'})
+        self.assertGreaterEqual(len(os.listdir(f'{vid}/video')), videos)
+        self.assertGreaterEqual(len(os.listdir(f'{vid}/audio')), audios)
+        shutil.rmtree(vid)
 
-    def test_default(self):
+    def test_av(self):
         mdl.download('av7')
         self.assertion('av7')
+
+    def test_av_URLs(self):
+        mdl.download('https://www.bilibili.com/video/av7')
+        self.assertion('av7')
+        mdl.download('https://www.bilibili.com/av7')
+        self.assertion('av7')
+        mdl.download('https://bilibili.com/video/av7')
+        self.assertion('av7')
+        mdl.download('http://www.bilibili.com/video/av7')
+        self.assertion('av7')
+        mdl.download('http://bilibili.com/video/av7')
+        self.assertion('av7')
+
+    def test_BV(self):
+        mdl.download('BV1XW411M7Gu')
+        self.assertion('BV1XW411M7Gu', 4, 2)
+
+    def test_BV_URLs(self):
+        mdl.download('https://www.bilibili.com/video/BV1xx411c7m9')
+        self.assertion('BV1xx411c7m9')
+        mdl.download('https://www.bilibili.com/BV1xx411c7m9')
+        self.assertion('BV1xx411c7m9')
+        mdl.download('https://bilibili.com/video/BV1xx411c7m9')
+        self.assertion('BV1xx411c7m9')
+        mdl.download('http://www.bilibili.com/video/BV1xx411c7m9')
+        self.assertion('BV1xx411c7m9')
+        mdl.download('http://bilibili.com/video/BV1xx411c7m9')
+        self.assertion('BV1xx411c7m9')
 
     def test_aria2(self):
         mdl.download('av7', 'aria2')
